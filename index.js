cba816293f69561b8cd3fde87e1de34079845a32
SirPotatoIV <otoolej33@gmail.com>
2019-12-24 12:52:14 -0600

    Completed present.

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 function main(){
-    console.log("Merrrrry Christmas!");
+    const boxEl = document.querySelector('.box')
+    boxEl.addEventListener('click', function(){
+        const gifts = ['coal', 'that semi-colon you forgot', 'no errors on the first try'];
+        const giftSelector = Math.floor(Math.random()*gifts.length);
+        const randomGift = gifts[giftSelector];
+        // console.log(giftSelector);
+        alert(`It's ${randomGift}. Merry Christmas ya filthy animal!`);
+    })
+    console.log(boxEl)
 }
 main();
